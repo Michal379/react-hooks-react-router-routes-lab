e7ab5e84b104c0ad08f1502fed1880ead3fb3cd5
Michal <khakaimic4@gmail.com>
2023-04-26 03:07:20 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (  
+  <div>
+    <NavBar>
+      <NavLink exact to = "/">Home</NavLink>
+      <NavLink to = "/movies">Movies</NavLink>
+      <NavLink to = "/directors">Directors</NavLink>
+      <NavLink to = "/actors">Actors</NavLink>
+      </NavBar>
+     </div>
+  );
 }
 
 export default NavBar;
